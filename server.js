cd7d49150fb60fa022a0f8a11af313160a9e1fc4
Gloriouspete <105151046+Gloriouspete@users.noreply.github.com>
2023-05-27 21:57:59 +0100

    Update server.js

--- server.js
+++ server.js
@@ -49,8 +49,8 @@ app.get('/reddit', async (req, res) => {
         <p class="ptitle">${title}</p>
         <h3>Subreddit</h3>
         <p class="pnumber"> ${subreddit}</p>
-         <h3>Link Url</h3>
-        <p>Link: <a href="${url}" target="_blank">${url}</a></p>
+        <h3>Link Url</h3>
+        <p><a href="${url}" target="_blank">${url}</a></p>
         <h3>Main Content</h3>
         <p class="body">${content}</p>
         <h3>Number of Comments</h3>
@@ -87,7 +87,8 @@ const searchReddit = async (query) => {
       title: post.title,
       content: post.selftext,
       comments: post.num_comments,
-      upvotes: post.ups
+      upvotes: post.ups,
+      url: `https://www.reddit.com/r/${post.subreddit}/comments/${post.id}`
     }));
   } catch (error) {
     console.error('Error searching Reddit:', error);
